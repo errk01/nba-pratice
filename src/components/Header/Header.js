84e2db1623435fe70b314ddea206b0ecb1860084
Unknown <erik.taylor30@gmail.com>
2018-12-24 13:51:40 -0700

    updated css modules

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,37 @@
-import React from 'react'
-import style from './header.css'
+import React from 'react';
+import style from './header.css';
+import { Link } from 'react-router-dom';
+
+import FontAwesome from 'react-fontawesome';
 
 const Header = () => {
+  
+  const navBars = () => (
+    <div className={style.bars}>
+      <FontAwesome
+        name="bars"
+        style={{
+          color: '#dfdfdf',
+          padding: '10px',
+          cursor: 'pointer'
+        }}
+      />
+    </div>
+  );
+  const logo = () => (
+    <Link to="/" className={style.logo}>
+      <img alt="nba logo" src="/images/nba_logo.png" />
+    </Link>
+  );
+
   return (
     <header className={style.header}>
-    header
       <div className={style.headerOpt}>
-Headser
+        {navBars()}
+        {logo()}
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
